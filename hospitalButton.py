8584c34025b61715a3c6145d54768af4e070093c
Adrian Ackon <adrianackon@rocketmail.com>
2019-07-25 17:31:16 +0000

    Changed the backend to allow a print after the post

--- hospitalButton.py
+++ hospitalButton.py
@@ -1,5 +1,6 @@
 from gpiozero import Button, Buzzer
 from postJsonWithFlask import PostWithJson
+import time
 
 global button
 global buzzer
@@ -9,8 +10,9 @@ buzzer = Buzzer(17)
 def pressButton():
     buzzer.on()
     PostWithJson()
+    time.sleep(5)
     buzzer.off()
-    return "Posted Successfully"
+    print ("Posted Successfully")
 
 while True:
     button.when_pressed = pressButton
